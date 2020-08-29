c7482d16b3b67b26909f6ae0b36d0ca3855a7a09
Saurabh Goel <goel.123.saurabh@gmail.com>
2020-08-29 19:28:19 +0530

    Updates

---
+++ precache-manifest.82f0377f3d28434b13abc4138841ce93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a18bc5bd2dd239c6002334673a1083",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "d88e3d86f6bc30607957",
+    "url": "/tictactoe/static/css/main.088b981c.chunk.css"
+  },
+  {
+    "revision": "cb0f0f7a2d179e76fb40",
+    "url": "/tictactoe/static/js/2.a6c13bc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.a6c13bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88e3d86f6bc30607957",
+    "url": "/tictactoe/static/js/main.92ccadc5.chunk.js"
+  },
+  {
+    "revision": "e8a4dc16f9ed24b827ee",
+    "url": "/tictactoe/static/js/runtime-main.6eda5305.js"
+  }
+]);
